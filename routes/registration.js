274004ea2e2bd53232b4a4ae9769da0aece1d188
Carlos Augusto <carlos.xell.3@gmail.com>
2025-04-26 08:28:58 -0300

    FEATURE: added new files to separate responsibilities

--- routes/registration.js
+++ routes/registration.js
@@ -2,27 +2,16 @@ let express = require('express');
 let router = express.Router();
 
 const APP = require('../src/app.js');
-const { createApp } = require('vue');
+const template = require('../src/template.js');
+const { createSSRApp } = require('vue');
 const { renderToString } = require('vue/server-renderer');
 
 
 router.get('/', async (req, res, next) => {
-	const appInstance = createApp(APP);
+	const appInstance = await createSSRApp(APP);
 	const AppHTML = await renderToString(appInstance);
 
-	const HTML = `
-           <!DOCTYPE html>
-           <html>
-           <head>
-               <title>Página Vue com Express</title>
-           </head>
-           <body>
-               ${AppHTML}
-               <script src="https://cdn.jsdelivr.net/npm/vue@3.3.4/dist/vue.global.js"></script>
-           </body>
-           </html>
-       `;
-
+	const HTML = template.render(AppHTML);
 	res.send(HTML);
 });
 
--- src/app.js
+++ src/app.js
@@ -1,12 +1,34 @@
-const { createApp } = require('vue');
+const {
+	computed,
+	onMounted,
+	ref
+} = require('vue');
 
 const APP = {
-	data() {
+	name: 'App',
+	props: {},
+	emits: [],
+	setup(props, { emit }) {
+		const message = ref('Olá Mundo !');
+		const counter = ref(0);
+		const messageCounter = computed(() => `${message.value} - Contador com valor em ${counter.value}`);
+
+		const updateCounter = () => counter.value++;
+
 		return {
-			message: 'Olá, mundo!'
+			message,
+			counter,
+			messageCounter,
+			updateCounter
 		};
 	},
-	template: '<div>{{ message }}</div>'
+	template: `
+    <div class="" id="app">
+			<h1 class="">{{ message }}</h1>
+			<p class="">{{ messageCounter }}</p>
+			<button @click="updateCounter">Update counter</button>
+		</div>
+	`
 };
 
 module.exports = APP;
